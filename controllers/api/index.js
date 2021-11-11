26c2d4850f2d0f70089a3027f78d7a094a81db24
joshuaramat <87837577+joshuaramat@users.noreply.github.com>
2021-11-11 15:33:57 -0800

    removed seed script

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const commentRoutes = require('./comment-routes');
 const postRoutes = require('./post-routes');
 const userRoutes = require('./user-routes');
-const searchRoutes = require('./search-routes')
 
 router.use('/comments', commentRoutes);
 router.use('/posts', postRoutes);
